669e06532ca26991ffd9a56503d9f37b6a48a862
Pedro Adrian Pereira Martinez <pedro.martinez@virtus.ufcg.edu.br>
2024-03-23 21:16:49 -0300

    Added encription and decription to server-client communication

--- TCPClient.py
+++ TCPClient.py
@@ -1,4 +1,5 @@
 from socket import *
+from feistelcipher import FeistelCipher
 
 serverName = '127.0.0.1'
 serverPort = 12000
@@ -9,7 +10,13 @@ clientSocket.connect((serverName,serverPort))
 
 #Recebe mensagem do usuario e envia ao servidor
 message = input('Digite uma frase: ')
-clientSocket.send(message.encode('ascii'))
+
+#Chave gerada usando função generate_secure_key em feistelcipher
+key = '1000100000100011110101001111100010010001011010101100001110100010'
+cipher = FeistelCipher(key)
+encripted_message = cipher.encrypt(message)
+
+clientSocket.send(encripted_message.encode('UTF-8'))
 
 #Aguarda mensagem de retorno e a imprime
 modifiedMessage, addr = clientSocket.recvfrom(2048)
--- TCPServer.py
+++ TCPServer.py
@@ -1,4 +1,5 @@
 from socket import *
+from feistelcipher import FeistelCipher
 
 serverPort = 12000
 #Cria o Socket TCP (SOCK_STREAM) para rede IPv4 (AF_INET)
@@ -9,10 +10,16 @@ serverSocket.listen(1)
 
 print("Servidor pronto para receber mensagens. Digite Ctrl+C para terminar.")
 
+#Chave gerada usando função generate_secure_key em feistelcipher
+key = '1000100000100011110101001111100010010001011010101100001110100010'
+cipher = FeistelCipher(key)
+
 while 1:
        #Cria um socket para tratar a conexao do cliente
      connectionSocket, addr = serverSocket.accept()
      sentence = connectionSocket.recv(1024)
-     capitalizedSentence = sentence.upper()
-     connectionSocket.send(capitalizedSentence)
+     decoded_sentence = sentence.decode('UTF-8')
+     decripted_sentence = cipher.decrypt(decoded_sentence)
+     capitalizedSentence = decripted_sentence.upper()
+     connectionSocket.send(capitalizedSentence.encode('UTF-8'))
      connectionSocket.close()
